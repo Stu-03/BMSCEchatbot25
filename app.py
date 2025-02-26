c9456c03bc42aaaddea16adc74428f256ed5c616
vivi03 <156214051+Stu-03@users.noreply.github.com>
2025-02-26 23:07:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,16 +16,16 @@ T2 = st.secrets["TKEY2"]
 tavkey = [T1, T2]
 HF_API_KEY = st.secrets["HF_API_KEY"]
 
+import sys
 import subprocess
 
-# Function to ensure the model is installed
 def ensure_spacy_model():
     try:
         nlp = spacy.load("en_core_web_sm")
         return nlp
     except OSError:
         print("Downloading spaCy model...")
-        subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"], check=True)
+        subprocess.run([sys.executable, "-m", "spacy", "download", "en_core_web_sm"], check=True)
         nlp = spacy.load("en_core_web_sm")  # Try loading again after installation
         return nlp
 
